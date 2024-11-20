1238b67cbf47d6b067634d71fcfef6144a2bc3ba
Felipe Barroso <72608109+felipebarrososa@users.noreply.github.com>
2024-11-20 18:46:52 -0300

    ajuste na query da movimentação financeira

--- src/routes/dashboard.js
+++ src/routes/dashboard.js
@@ -35,7 +35,15 @@ router.get('/', async (req, res) => {
                                                 FROM public.inscricao_geral ig
                                                 LEFT JOIN public.localidades l ON ig.localidade_id = l.id
         `);
-        const movimentacaoFinanceira = await pool.query('SELECT id, descricao, valor FROM public.movimentacao_financeira');
+        const movimentacaoFinanceira = await pool.query(`SELECT 
+                                                            mf.id,
+                                                            CONCAT('Pagamento referente à localidade: ', loc.nome) AS descricao,
+                                                            mf.valor
+                                                        FROM 
+                                                            public.movimentacao_financeira mf
+                                                        LEFT JOIN 
+                                                            public.localidades loc ON CAST(SUBSTRING(mf.descricao FROM 'ID: (\d+)') AS INT) = loc.id;
+                                                        `);
         const pagamento = await pool.query(`
             SELECT p.id, p.valor_pago, l.nome AS localidade
             FROM public.pagamento p
