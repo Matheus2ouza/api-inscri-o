0fd1ebcd782153898f411a36c0c5736eb9e3c6fa
Matheus2ouza <matifurtado0@gmail.com>
2024-12-05 06:43:47 -0300

    pegando o nome do arquivo para a query

--- src/routes/imagemPayment.js
+++ src/routes/imagemPayment.js
@@ -9,13 +9,26 @@ const upload = multer({ storage });
 
 // Rota para registrar o comprovante de pagamento
 router.post('/', upload.single('comprovante_pagamento'), async (req, res) => {
-    const { cidade } = req.body;
+    const { originalname } = req.file; // Nome original do arquivo
     const comprovante_pagamento = req.file ? req.file.buffer : null;  // Dados binários da imagem (sem conversão para base64)
+    const tipo_arquivo = req.file ? req.file.mimetype : null;  // Tipo MIME do arquivo
 
     // Verifica se o comprovante foi carregado
     if (!comprovante_pagamento) {
         console.warn('Comprovante de pagamento não fornecido.');
         return res.status(400).json({ message: 'Comprovante de pagamento é obrigatório.' });
+    }
+
+    // Extrai a cidade do nome do arquivo usando expressão regular
+    const regex = /comprovante_\d+_(.+?)\.\w+$/;
+    const match = originalname.match(regex);
+    let cidade = null;
+
+    if (match && match[1]) {
+        cidade = match[1];  // Captura o nome da cidade
+    } else {
+        console.warn('Nome do arquivo não segue o formato esperado.');
+        return res.status(400).json({ message: 'Nome do arquivo não segue o formato esperado.' });
     }
 
     try {
@@ -37,8 +50,8 @@ router.post('/', upload.single('comprovante_pagamento'), async (req, res) => {
 
         // Insere o comprovante de pagamento na tabela "comprovantes"
         const result = await pool.query(
-            'INSERT INTO comprovantes (comprovante_imagem, localidade_id) VALUES ($1, $2) RETURNING id',
-            [comprovante_pagamento, localidade_id]  // Dados binários diretamente
+            'INSERT INTO comprovantes (comprovante_imagem, tipo_arquivo, localidade_id) VALUES ($1, $2, $3) RETURNING id',
+            [comprovante_pagamento, tipo_arquivo, localidade_id]  // Dados binários e tipo MIME diretamente
         );
 
         const comprovanteId = result.rows[0].id;
