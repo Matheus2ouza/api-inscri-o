02de73c2bf8714fb3fa87c2365b34e337a019dff
MatheusSouza <matheusfurtadogg@gmail.com>
2025-02-08 09:34:57 -0300

    add do pagamentos

--- src/routes/createPdf.js
+++ src/routes/createPdf.js
@@ -81,6 +81,7 @@ createPdfRouter.post("/createPdf", async (req, res) => {
             "Inscrição": dataInscricao,
             "Inscrição Avulsa": dataInscricaoAvulsa,
             "Tickets": dataTicket,
+            "Pagamentos": Pagamentos,
             "Movimentação": dataMovimentacao
         };
         
