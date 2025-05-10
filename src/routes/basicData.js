897885b7f06820f24911a9544a802ddcd481a5b6
Matheus2ouza <matheusfurtadogg@gmail.com>
2025-05-10 13:26:00 -0300

    mudança definitiva da rota localidades

--- src/routes/basicData.js
+++ src/routes/basicData.js
@@ -4,24 +4,6 @@ const { pool } = require('../db/dbConnection')
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
-// router.get('/localidades', async (req, res) => {
-//     try {
-//         const locations = await pool.query('SELECT * FROM localidades');
-//         const result = locations.rows;
-
-//         if (result.length === 0) { // Verifica se não há localidades
-//             console.warn('Consulta de localidade feita, mas não teve nenhum resultado.');
-//             res.status(404).json({ message: 'Nenhuma localidade encontrada.' }); // Código 404 para não encontrado
-//         } else {
-//             console.info('Consulta de localidade feita com sucesso.');
-//             res.status(200).json(result); // Código 200 para OK
-//         }
-//     } catch (err) {
-//         console.error(`Erro ao buscar localidade: ${err}`);
-//         res.status(500).json({ error: 'Erro ao buscar localidade.' });
-//     }
-// });
-
 router.get('/localidades', async (req, res) => {
     try {
         const locations = await prisma.localidades.findMany({
