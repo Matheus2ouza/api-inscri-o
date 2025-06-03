919a50ff019d518850d206b4fc602628aa67955d
Matheus2ouza <matheusfurtadogg@gmail.com>
2025-06-03 16:41:22 -0300

    ajuste da configuração do redis

--- src/lib/redis.js
+++ src/lib/redis.js
@@ -2,5 +2,11 @@ import { Redis } from "@upstash/redis";
 
 export const redis = new Redis({
        url: process.env.KV_REST_API_URL,
-       token: process.env.KV_REST_API_TOKEN
+       token: process.env.KV_REST_API_TOKEN,
+       timeout: 10000, // 10 seconds
+       enableTelemetry: false,
+       retryStrategy: (attempt) =>{
+              if (attempt > 3) return null;
+              return attempt * 1000; // Retry after 1 second, 2 seconds, etc.
+       }
 })
