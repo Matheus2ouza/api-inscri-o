8711343fcc9522feee6fefe321e4c5d0010e55bb
matheus2ouza <matheusfurtadogg@gmail.com>
2025-07-01 05:01:52 -0300

    fix: import authorizeRole middleware in localityRouter

--- src/routes/localityRouter.js
+++ src/routes/localityRouter.js
@@ -1,6 +1,7 @@
 
 const express = require('express');
 const { body } = require('express-validator');
+const { authorizeRole} = require('../middlewares/authMiddleware')
 const localityController = require('../controllers/localityController')
 
 const router = express.Router();
