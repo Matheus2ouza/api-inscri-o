a4deb70f30e84d01777e8ab431cf3ae071fa1d13
matheus2ouza <matheusfurtadogg@gmail.com>
2025-07-01 05:19:11 -0300

    fix: add authentication middleware to ListLocality route

--- src/routes/localityRouter.js
+++ src/routes/localityRouter.js
@@ -1,12 +1,12 @@
 
 const express = require('express');
 const { body } = require('express-validator');
-const { authorizeRole} = require('../middlewares/authMiddleware')
+const { authorizeRole, authenticateToken} = require('../middlewares/authMiddleware')
 const localityController = require('../controllers/localityController')
 
 const router = express.Router();
 
-router.get('/ListLocality', authorizeRole("admin"), localityController.listLocality)
+router.get('/ListLocality',authenticateToken, authorizeRole("admin"), localityController.listLocality)
 
 
 module.exports = router
