9ad0152dfa1be1b133e45fdb0710bf21abb4ddc8
matheus2ouza <matheusfurtadogg@gmail.com>
2025-07-03 02:52:02 -0300

    fix: adjust error line tracking in uploadFile function for improved accuracy

--- src/controllers/registerController.js
+++ src/controllers/registerController.js
@@ -90,7 +90,7 @@ exports.uploadFile = async (req, res) => {
       const tipoInscricao = item["Tipo de Inscrição"];
 
       if(!nome || !nascimento || !sexo || !tipoInscricao) {
-        lineError.push(index + 3);
+        lineError.push(index + 5);
         return;
       }
       
